3bd484d2dbc0e7af3039ac4d13c3f3a0c2278830
Afterbanks <dlozanolu@gmail.com>
2016-04-28 15:59:42 +0200

    Update call the api.php

--- call the api.php
+++ call the api.php
@@ -3,12 +3,12 @@ $postString = "servicekey=<servicekey>&service=<bank name>&documentType=1&user=<
 $json = getJson($postString);
 
 $array = json_decode($json,true);
-[...]
+print_r ($array);
 
 function getJson($postString=''){
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
   curl_setopt($ch, CURLOPT_URL, 'https://www.afterbanks.com/endpoint/');
   curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate'); 
   curl_setopt($ch, CURLOPT_POST, 1);
